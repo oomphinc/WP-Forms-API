56ba2b42f4c1281b771ac594fa710b19e7ad3e21
Ben Doherty <ben@thinkoomph.com>
2015-10-22 14:27:36 -0400

    First pass at getting this managed by compsoer

---
+++ src/wp-forms-api.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Plugin Name: WP Forms API
+Description: Implement an API similar to Drupal's Forms API for rendering
+and processing forms.
+Author: Ben Doherty @ Oomph, Inc.
+Version: 0.4.3
+Author URI: http://www.oomphinc.com/thinking/author/bdoherty/
+License: GPLv2 or later
+
+		Copyright © 2015 Oomph, Inc. <http://oomphinc.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Ensure only one instance of the WP_Forms_API is loaded at a time
+if ( ! class_exists( 'WP_Forms_API' ) ) {
+    require( __DIR__ . '/class-wp-forms-api.php' );
+}
