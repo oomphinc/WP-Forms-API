b5f3f6fefc4b34374b391083f57adbc72de2b00c
Ben Doherty <ben@thinkoomph.com>
2016-03-23 10:11:20 -0400

    Make test file 5.3 compatible, too!

--- tests/test-wp-forms-api.php
+++ tests/test-wp-forms-api.php
@@ -2,11 +2,10 @@
 
 class WP_Forms_API_Test extends WP_UnitTestCase {
 	function test_render() {
-		$values = [];
-		$html = WP_Forms_API::render_form( [
+		$values = array();
+		$html = WP_Forms_API::render_form( array(
 			'text-input' => [ '#type' => 'text' ]
-		], $values );
-
+		), $values );
 
 		$this->assertContains( 'type="text"', $html );
 		$this->assertContains( 'name="text-input"', $html );
